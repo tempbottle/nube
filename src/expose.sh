5ed1fbac0f2b1b08070de20954d049bf54232903
Ariel <amonaco@gmail.com>
2015-01-29 19:21:08 +1100

    proof-of-concept

--- src/expose.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash - 
-g++ -I/usr/local/src/v8/include/ expose.cpp -o expose -Wl,--start-group /usr/local/src/v8/out/native/obj.target/{tools/gyp/libv8_{base,libbase,snapshot,libplatform},third_party/icu/libicu{uc,i18n,data}}.a -Wl,--end-group -lrt -pthread
